41551b69a23bb63a6d357d491047a6b9f930b5f0
dengshilong <dengshilong1988@gmail.com>
2017-03-01 17:06:09 +0800

    fix: add env config

--- manage.py
+++ manage.py
@@ -5,6 +5,13 @@ from app.models import User, Post
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
+if os.path.exists('.env'):
+    print('Importing environment from .env...')
+    for line in open('.env'):
+        var = line.strip().split('=')
+        if len(var) == 2:
+            os.environ[var[0]] = var[1]
+
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
