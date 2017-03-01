578ffa38707bd4b2ef4e120b848444bf0c7e5e9a
dengshilong <dengshilong1988@gmail.com>
2017-03-01 20:17:22 +0800

    fix: add fabfile.py

---
+++ fabfile.py
@@ -0,0 +1,8 @@
+from fabric.api import *
+env.use_ssh_config = True
+env.hosts = ['dsl']
+
+def deploy():
+    code_dir = '~/program/python/flask-lovehate'
+    with cd(code_dir):
+        run("git pull")
