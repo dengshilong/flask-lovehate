3aa8d512fc3467fbda7c99bbea79016aae2c69ce
dengshilong <dengshilong1988@gmail.com>
2017-03-10 17:29:23 +0800

    fix: fix import order

--- fabfile.py
+++ fabfile.py
@@ -2,8 +2,11 @@ from fabric.api import *
 env.use_ssh_config = True
 env.hosts = ['dsl']
 
+
 def deploy():
     code_dir = '~/program/python/flask-lovehate'
     with cd(code_dir):
         run("git pull")
+        run("~/.pyenv/versions/lovehate/bin/pip install -r requirements.txt")
+        run("~/.pyenv/versions/lovehate/bin/python manage.py db upgrade")
         run("sudo supervisorctl restart lovehate")
--- manage.py
+++ manage.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
+# pylint: disable=wrong-import-position
 import os
-from app import create_app, db
-from app.models import User, Post, Category
-from flask_script import Manager, Shell
-from flask_migrate import Migrate, MigrateCommand
-
 if os.path.exists('.env'):
     print('Importing environment from .env...')
     for line in open('.env'):
         var = line.strip().split('=')
         if len(var) == 2:
             os.environ[var[0]] = var[1]
+from app import create_app, db
+from app.models import User, Post, Category
+from flask_script import Manager, Shell
+from flask_migrate import Migrate, MigrateCommand
 
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
